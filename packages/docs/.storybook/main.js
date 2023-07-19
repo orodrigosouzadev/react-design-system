b769420fcb9fbc0f3437917b44d5275d66ed5c80
Rodrigo Souza <rodrigo@rodrigosouza.dev>
2023-07-19 16:44:04 -0300

    fix: fix versions problems

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,25 +1,26 @@
-/** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
-  addons: [
+module.exports = {
+  "stories": [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
+    "@storybook/addon-a11y"
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "@storybook/builder-vite"
   },
-  docs: {
-    autodocs: true,
+  "features": {
+    "storyStoreV7": true
   },
   viteFinal: (config, { configType }) => {
-    if (configType === "PRODUCTION") {
-      config.base = '/react-design-system/';
+    if (configType === 'PRODUCTION') {
+      config.base = '/react-design-system/'
     }
 
-    return config;
+    return config
   }
-};
-export default config;
+}
